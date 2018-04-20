5988c04a3b0cbe994ea2fb7401fbf408910586fb
Eric Kim <ericjkim9@gmail.com>
2018-04-20 16:25:56 -0700

    checking readme

--- gen_md.sh
+++ gen_md.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 #source: https://stackoverflow.com/questions/23989232/is-there-a-way-to-represent-a-directory-tree-in-a-github-readme-md
-tree=$(tree -tf --noreport -I '*~' --charset ascii $1 |
-       sed -e 's/| \+/  /g' -e 's/[|`]-\+/ */g' -e 's:\(* \)\(\(.*/\)\([^/]\+\)\):\1[\4](\2):g')
+#tree=$(tree -tf --noreport -I '*~' --charset ascii $1 |
+#       sed -e 's/| \+/  /g' -e 's/[|`]-\+/ */g' -e 's:\(* \)\(\(.*/\)\([^/]\+\)\):\1[\4](\2):g')
 
-printf '## Problems  \n\n```  ' > tree.md
-tree >> tree.md
-echo '```  ' >> tree.md
+#printf '## Problems  \n\n```  ' > tree.md
+#tree >> tree.md
+#echo '```  ' >> tree.md
+md-file-tree > tree.md
 
 cat code_journal.md tree.md > readme.md
